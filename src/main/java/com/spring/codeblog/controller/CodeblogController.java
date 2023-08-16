89a537f668d4a6fc48629e2a434eb9c4f304431f
Perkles <victor.coelho@discente.ufma.br>
2023-08-16 17:24:34 -0300

    busca de post por id

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,4 +31,19 @@ public class CodeblogController {
         mv.addObject("posts", posts);
         return mv;
     }
+	
+	
+	/* buscar post por id*/
+		@GetMapping(value="/{id}")
+	    public ModelAndView getPostDetails(@PathVariable("id") long id){
+	        ModelAndView mv = new ModelAndView("postDetails"); //pagina html retornada
+	        Post post = codeblogService.findById(id);
+	        mv.addObject("post", post);
+	        return mv;
+	    }
+	
+	
+	
+	
+	
 }
