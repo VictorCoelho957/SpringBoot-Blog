6aa8b6d86f84d368231fc654f21a62429b5b0ef2
Perkles <victor.coelho@discente.ufma.br>
2023-08-22 13:57:04 -0300

    atualização dos métodos da classe controller

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -1,49 +1,69 @@
 package com.spring.codeblog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
 
+import jakarta.validation.Valid;
 
-@RestController
-@RequestMapping(value="/posts")
+@Controller
 public class CodeblogController {
+  
 	
-	//ineçao da service
-	
-	@Autowired
-	CodeblogService codeblogService;
-	
-	
-	//metodo para buscar posts 
-	@GetMapping
+	//inserçao da service
+    @Autowired
+    CodeblogService codeblogService;
+
+
+  //metodo para buscar posts 
+    @RequestMapping(value="/posts", method=RequestMethod.GET)
     public ModelAndView getPosts(){
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = codeblogService.findAll();
         mv.addObject("posts", posts);
         return mv;
     }
-	
-	
+
+    
+
 	/* buscar post por id*/
-		@GetMapping(value="/{id}")
-	    public ModelAndView getPostDetails(@PathVariable("id") long id){
-	        ModelAndView mv = new ModelAndView("postDetails"); //pagina html retornada
-	        Post post = codeblogService.findById(id);
-	        mv.addObject("post", post);
-	        return mv;
-	    }
-	
-	
-	
-	
-	
+    @RequestMapping(value="/posts/{id}", method=RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id){
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = codeblogService.findById(id);
+        mv.addObject("post", post);
+        return mv;
+    }
+
+    
+    /*metodo para criar um nobo post pelo formulario */
+    @RequestMapping(value="/newpost", method=RequestMethod.GET)
+    public String getPostForm(){
+        return "postForm";
+    }
+    
+    @RequestMapping(value="/newpost", method=RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){   //@Valid serve para confirmação as anotaçõe feitas na classe Post, que evita por exemplo, campos em brancos
+        if(result.hasErrors()){
+            attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");  ///captura algum e volta para o usuario preencher novamente
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        codeblogService.save(post);
+        return "redirect:/posts";
+    }
+    
+
+   
 }
